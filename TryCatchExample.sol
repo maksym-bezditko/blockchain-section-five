dae03effc095485774c95cc9dee3103f3e6fa496
Max Bezditko <mb.preply.1@gmail.com>
2024-07-03 13:29:16 +0300

    add error handling examples

---
+++ TryCatchExample.sol
@@ -0,0 +1,21 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.14;
+
+contract WillThrow {
+    function aFunction() public pure {
+        require(false, "Error message");
+    }
+}
+
+contract ErrorHandling {
+    event ErrorLogging(string reason);
+    function catchError() public {
+        WillThrow will = new WillThrow();
+        try will.aFunction() {
+            //here we could do something if it works
+        } catch Error(string memory reason) {
+            emit ErrorLogging(reason);
+        }
+    }
+}
